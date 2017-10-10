08c2848b18c5cca8e0de1afbb12d60cc0c7d664e
HanaTuSae <zhengdingdong0307@qq.com>
2017-10-11 02:25:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
          '/api':{
-  target:'https://hanatusae.github.io/api',
+  target:'http://localhost:8080/api',
   changeOrigin:true,
   pathRewrite:{
     '^/api':''
