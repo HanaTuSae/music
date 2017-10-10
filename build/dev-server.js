307c09e7450da927dfabb2d4e315e0e3fa1b16da
HanaTuSae <zhengdingdong0307@qq.com>
2017-10-11 02:19:22 +0800

    Update dev-server.js

--- build/dev-server.js
+++ build/dev-server.js
@@ -248,7 +248,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'https://hanatusae.github.io/:' + port
+var uri = 'https://localhost:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
